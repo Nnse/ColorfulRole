df6d1cfcee8c8c495c887f30a1ff17d22a32811e
奈々瀬 <107156665+Nnse@users.noreply.github.com>
2022-06-17 11:13:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,119 @@
-from discord.ext import commands
+import asyncio
+import tkinter
 from os import getenv
-import traceback
+from tkinter import colorchooser
+from typing import Union
 
-bot = commands.Bot(command_prefix='/')
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=';', intents=intents)
 
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+async def on_ready():
+    await asyncio.sleep(5)
+    await bot.change_presence(activity=discord.Game(name=str(len(bot.guilds)) + ' servers'))
+
+
+@bot.event
+async def on_guild_join(guild):
+    await asyncio.sleep(5)
+    await bot.change_presence(activity=discord.Game(name=str(len(bot.guilds)) + ' servers'))
+
+
+@bot.event
+async def on_guild_remove(guild):
+    await asyncio.sleep(5)
+    await bot.change_presence(activity=discord.Game(name=str(len(bot.guilds)) + ' servers'))
+
+
+@bot.command(aliases=['p'])
+async def pallet(ctx, target: str = ''):
+    target_user: discord.Member = ctx.author
+    if ctx.author.guild_permissions.administrator:
+        if target != '':
+            user = await fetch_target_user(ctx.guild, target)
+            if user is not None:
+                target_user = user
+
+    await quit_hex_role(target_user)
+
+    hex_str = get_color_from_pallet()
+    role = await fetch_hex_role(ctx.guild, hex_str)
+    await target_user.add_roles(role)
+
+
+@bot.command(aliases=['c'])
+async def color(ctx, hex_str: str, target: str = ''):
+    target_user: discord.Member = ctx.author
+    if ctx.author.guild_permissions.administrator:
+        if target != '':
+            user = await fetch_target_user(ctx.guild, target)
+            if user is not None:
+                target_user = user
+
+    await quit_hex_role(target_user)
+
+    if '#' not in hex_str:
+        return
+    role = await fetch_hex_role(ctx.guild, hex_str)
+    await target_user.add_roles(role)
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def reset(ctx, target: str = ''):
+    target_user: discord.Member = ctx.author
+    if ctx.author.guild_permissions.administrator:
+        if target != '':
+            user = await fetch_target_user(ctx.guild, target)
+            if user is not None:
+                target_user = user
+
+    await quit_hex_role(target_user)
+    
+
+def get_color_from_pallet() -> str:
+    root = tkinter.Tk()
+    root.withdraw()
+    root.attributes("-topmost", True)
+    c = colorchooser.askcolor(parent=root)
+    if color is not None:
+        root.destroy()
+    root.mainloop()
+
+    return str(c[1])
+
+
+async def fetch_target_user(guild: discord.Guild, text: str) -> Union[discord.Member, None]:
+    if text.isdigit():
+        return await guild.fetch_member(int(text))
+    else:
+        if '@' in text:
+            id_str = ((text.replace('<', '')).replace('@', '')).replace('>', '')
+            return await guild.fetch_member(int(id_str))
+    return None
+
+
+async def fetch_hex_role(guild: discord.Guild, hex_str: str) -> discord.Role:
+    sixteen_integer_hex = int(hex_str.replace('#', ''), 16)
+    readable_hex = int(hex(sixteen_integer_hex), 0)
+
+    for role in guild.roles:
+        if role.name == hex_str:
+            return role
+
+    return await guild.create_role(name=hex_str, colour=readable_hex)
+
+
+async def quit_hex_role(target: discord.Member):
+    for role in target.roles:
+        if '#' in role.name:
+            if len(role.members) == 1:
+                await role.delete()
+
+            await target.remove_roles(role)
 
 
 token = getenv('DISCORD_BOT_TOKEN')
